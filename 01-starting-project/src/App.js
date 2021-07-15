a1c4cf87b2b63431b97c8ab2611b2027f4eac113
Georgi Ermenkov <germenkov@gmail.com>
2021-07-15 18:17:23 +0300

    Few basic components done

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,5 +1,18 @@
+import Backdrop from "./components/Backdrop";
+import ToDo from "./components/ToDo";
+import Modal from "./components/Modal";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <ToDo text='Learn React'/>
+      <ToDo text='Practice React'/>
+      <ToDo text='Master React'/>
+      <Modal />
+      <Backdrop></Backdrop>
+    </div>
+  )
 }
 
 export default App;
