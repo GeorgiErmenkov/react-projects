75c88fa7af3a124394f64963eebc48ab696be32d
Georgi Ermenkov <germenkov@gmail.com>
2021-07-18 18:11:45 +0300

    Basic routing structure and nav bar

--- meetup-app/src/App.js
+++ meetup-app/src/App.js
@@ -1,5 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavouritesPage from './pages/Favourite';
+import NewMeeetupPage from './pages/NewMeetup';
+import MainNavigation from './components/layouts/MainNavigation';
+
+
 function App() {
-  return <div></div>;
+  return <div>
+    <MainNavigation />
+    <Switch>
+      <Route path='/' exact={true} >
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeeetupPage />
+      </Route>
+      <Route path='/favourites'>
+        <FavouritesPage />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
---
+++ meetup-app/src/pages/NewMeetup.js
@@ -0,0 +1,9 @@
+function NewMeeetupPage() {
+    return(
+        <div>
+            New Meetups Page
+        </div>
+    )
+}
+
+export default NewMeeetupPage;
