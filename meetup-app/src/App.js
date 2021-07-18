db2fe6d3d879e5819862bec3b6c336010bd6b108
Georgi Ermenkov <germenkov@gmail.com>
2021-07-18 19:35:21 +0300

    Adds card-like styling and dummy data

--- meetup-app/src/App.js
+++ meetup-app/src/App.js
@@ -1,26 +1,27 @@
 import { Route, Switch } from 'react-router-dom';
+import Layout from './components/layouts/Layout';
 
 import AllMeetupsPage from './pages/AllMeetups';
 import FavouritesPage from './pages/Favourite';
 import NewMeeetupPage from './pages/NewMeetup';
-import MainNavigation from './components/layouts/MainNavigation';
+
 
 
 function App() {
-  return <div>
-    <MainNavigation />
-    <Switch>
-      <Route path='/' exact={true} >
-        <AllMeetupsPage />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeeetupPage />
-      </Route>
-      <Route path='/favourites'>
-        <FavouritesPage />
-      </Route>
-    </Switch>
-  </div>;
-}
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact={true} >
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeeetupPage />
+        </Route>
+        <Route path='/favourites'>
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  )}
 
-export default App;
+    export default App;
